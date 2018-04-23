88a422a74344f733ca1388d9380a5935c24a2fdb
na57 <na57@qq.com>
2018-04-23 13:28:19 +0800

    搭建express框架

--- index.mjs
+++ index.mjs
@@ -1,10 +1,20 @@
 import express from "express";
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 
-const host = 'localhost';
-const port = 3000;
+import { host, port, secret } from './config';
 
 const app = express();
 
+//
+// Register Node.js middleware
+// -----------------------------------------------------------------------------
+app.use(cookieParser(secret));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+
 app.listen(port, () => {
-  console.log(`The server is running at http://${host}:${port}`);
+  console.log(`The server is running at http://${host}`);
 });

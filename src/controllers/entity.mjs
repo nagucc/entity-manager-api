cccf14199cf93d4e7cfbc201c382ae9bf43a880e
na57 <na57@qq.com>
2018-04-23 16:08:29 +0800

    搭建完框架

--- src/controllers/entity.mjs
+++ src/controllers/entity.mjs
@@ -1,14 +1,23 @@
 import express from 'express';
 import expressJwt from 'express-jwt';
-
-import {expressJwtOptions} from '../utils';
+import managers from 'entity-manager';
+import { expressJwtOptions } from '../utils';
+import { mongoUrl } from '../config';
 
 const router = new express.Router();
 
-router.get('/', 
+router.get(
+  '/:collectionName',
   expressJwt(expressJwtOptions),
-  (req, res) => {
-    res.json({ ret: 0, data: req.user });
-  });
+  async (req, res) => {
+    const manager = new managers.EntityManagerMongoDB(mongoUrl, req.params.collectionName);
+    try {
+      const entities = await manager.find();
+      res.success(entities);
+    } catch (e) {
+      res.fail('server error', e);
+    }
+  },
+);
 
 export default router;
--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,10 @@
-import express from "express";
+import express from 'express';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 
 import { host, port, secret } from './config';
 
-import {entity} from './controllers';
+import { entity } from './controllers';
 
 const app = express();
 
@@ -16,6 +16,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
+app.use('*', (req, res, next) => {
+  res.success = data => res.json({
+    ret: 0,
+    data,
+  });
+  res.final = (msg, err) => {
+    res.json({
+      ret: -1,
+      msg,
+      err,
+    });
+  };
+  next();
+});
 app.use('/entity', entity);
 
 app.listen(port, () => {

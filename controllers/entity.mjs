7399cc82728893a71905ad4fa54b42720dfa5263
na57 <na57@qq.com>
2018-04-23 14:09:32 +0800

    变更路径

--- controllers/entity.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = new express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ ret: 0, data: 'hell, world' });
-});
-
-export default router;
---
+++ src/controllers/entity.mjs
@@ -0,0 +1,14 @@
+import express from 'express';
+import expressJwt from 'express-jwt';
+
+import {expressJwtOptions} from '../utils';
+
+const router = new express.Router();
+
+router.get('/', 
+  expressJwt(expressJwtOptions),
+  (req, res) => {
+    res.json({ ret: 0, data: 'hell, world' });
+  });
+
+export default router;

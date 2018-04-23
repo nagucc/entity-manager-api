5fcc7386d881004aaf5e9340f8534b2938f530ec
na57 <na57@qq.com>
2018-04-23 14:17:51 +0800

    搭建jwt环境

--- src/controllers/entity.mjs
+++ src/controllers/entity.mjs
@@ -8,7 +8,7 @@ const router = new express.Router();
 router.get('/', 
   expressJwt(expressJwtOptions),
   (req, res) => {
-    res.json({ ret: 0, data: 'hell, world' });
+    res.json({ ret: 0, data: req.user });
   });
 
 export default router;
